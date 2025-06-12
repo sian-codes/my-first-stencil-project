9f1cead969b9d2eeb69dbe54e77596dfd3ed1406
Siân <66971939+sian-codes@users.noreply.github.com>
2025-06-12 23:28:16 +0100

    initial project with comments to start learning stencil.js

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,12 +1,18 @@
 import { Component, Prop, h } from '@stencil/core';
 import { format } from '../../utils/utils';
 
+// This decorator provides metadata about the component to the Stencil compiler
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
   shadow: true,
 })
 export class MyComponent {
+
+  // the component contains three class members, first, middle and last.
+  // Each of these class members have the @Prop() decorator applied to them
+  // @Prop() tells Stencil that the property is public to the component,
+  // and allows Stencil to rerender when any of these public properties change
   /**
    * The first name
    */
